7b875f1197e45a3472ebd5ae8646484fea9796b9
JoshuaEnglish <Joshua.R.English@gmail.com>
2021-04-24 15:08:16 -0700

    documenting limit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # djangotest
 Troubleshooting Django issues
+
+
+## limit
+
+In my propriety app that I'm building for my employer, I ran into
+a problem where the admin screen is not limiting my choice in creating
+a sales team.
+
+This app builds up the basic model and does work in the basic app.
+
--- mysite/limit/models.py
+++ mysite/limit/models.py
@@ -1,13 +1,26 @@
+"""Models
+
+The problem I'm trying to recreate and solve is in the models below, the
+admin screen, when adding a team, does not limit the choices appropriately.
+"""
+
 from django.db import models
 
 # Create your models here.
 class Role(models.Model):
+    """
+    Role is a separate model because the full app caries extra 
+    information that is not relevant to this problem.
+    """
     name = models.CharField("Name", max_length=32)
 
     def __str__(self):
         return self.name
 
 class Person(models.Model):
+    """
+    Person - name and role as ForeignKey
+    """
     name = models.CharField("Name", max_length=64)
     role = models.ForeignKey(Role, on_delete=models.PROTECT)
 
@@ -15,6 +28,10 @@ class Person(models.Model):
         return f"{self.name} ({self.role.name})"
 
 class Team(models.Model):
+    """
+    Team - one sales team can have multiple sales reps and multiple
+    engineers, and a Person can be on multiple teams
+    """
     rep = models.ManyToManyField(
         Person,
         related_name = "rep_on",
